09b8c5a0470b909ff5d1307dfdd17f69a906e7b1
Emmanuel Zenderock <contact@zenderock.me>
2025-11-04 11:41:40 +0100

    Update repository owner and name in fetchRelease function

--- apps/web/src/queries/latest-release.ts
+++ apps/web/src/queries/latest-release.ts
@@ -8,8 +8,8 @@ const releaseKey = 'OCTOKIT_RELEASE'
 async function fetchRelease() {
   const octokit = new Octokit()
   const { data } = await octokit.repos.getLatestRelease({
-    owner: 'wannabespace',
-    repo: 'conar',
+    owner: 'servelink-deploy',
+    repo: 'tryquest',
   })
 
   return data
