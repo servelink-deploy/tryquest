020393dc107688c4235a0a94eba991394d4369d3
Emmanuel Zenderock <contact@zenderock.me>
2025-11-04 13:14:39 +0100

    fix button color bug

--- apps/web/src/components/download-button.tsx
+++ apps/web/src/components/download-button.tsx
@@ -5,7 +5,9 @@ export function DownloadButton({ fallback }: { fallback?: React.ReactNode }) {
 
   return (
     <Button size="lg" className="flex items-center justify-center gap-2 text-white">
-      <a href="https://tryquest-images.servel.ink/releases/TryQuest-Windows-0.23.0-Setup.exe">
+      <a href="https://tryquest-images.servel.ink/releases/TryQuest-Windows-0.23.0-Setup.exe" className='text-white' style={{
+        color: "white"
+      }}>
         Download for Windows
       </a>
     </Button>
